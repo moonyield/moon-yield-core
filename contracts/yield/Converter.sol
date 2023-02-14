6f7640e0e21271d38188be87fde0339d8b5bf2ef
manangouhari <gouhari21@hotmail.com>
2023-02-15 01:19:31 +0530

    :construction: stubs for WELl->USDC & GLMR->USDC swaps

--- contracts/yield/Converter.sol
+++ contracts/yield/Converter.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.17;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import {IWETH} from "./interfaces/IWETH.sol";
 import {IV2Router} from "./interfaces/dex/IV2Router.sol";
 import {IStableRouter} from "./interfaces/dex/IStableRouter.sol";
 import {ISwap} from "./interfaces/dex/ISwap.sol";
@@ -18,6 +19,7 @@ contract Converter {
         ERC20 axlUSDC;
         ERC20 USDC;
         ERC20 WELL;
+        IWETH WGLMR;
     }
     Tokens public tokens;
 
@@ -29,29 +31,30 @@ contract Converter {
 
     constructor() {
         routers.Stable = IStableRouter(
-            address(0xB0Dfd6f3fdDb219E60fCDc1EA3D04B22f2FFa9Cc)
+            0xB0Dfd6f3fdDb219E60fCDc1EA3D04B22f2FFa9Cc
         );
-        routers.V2 = IV2Router(
-            address(0x70085a09D30D6f8C4ecF6eE10120d1847383BB57)
-        );
+        routers.V2 = IV2Router(0x70085a09D30D6f8C4ecF6eE10120d1847383BB57);
 
-        tokens.axlUSDC = ERC20(
-            address(0xCa01a1D0993565291051daFF390892518ACfAD3A)
-        );
-        tokens.USDC = ERC20(
-            address(0x931715FEE2d06333043d11F658C8CE934aC61D0c)
-        );
-        tokens.WELL = ERC20(
-            address(0x511aB53F793683763E5a8829738301368a2411E3)
-        );
+        tokens.axlUSDC = ERC20(0xCa01a1D0993565291051daFF390892518ACfAD3A);
+        tokens.USDC = ERC20(0x931715FEE2d06333043d11F658C8CE934aC61D0c);
+        tokens.WELL = ERC20(0x511aB53F793683763E5a8829738301368a2411E3);
 
-        pools.axlUSDC4pool = ISwap(
-            address(0xA1ffDc79f998E7fa91bA3A6F098b84c9275B0483)
-        );
-        pools.base4pool = ISwap(
-            address(0xB1BC9f56103175193519Ae1540A0A4572b1566F6)
-        );
+        tokens.WGLMR = IWETH(0xAcc15dC74880C9944775448304B263D191c6077F);
+
+        pools.axlUSDC4pool = ISwap(0xA1ffDc79f998E7fa91bA3A6F098b84c9275B0483);
+        pools.base4pool = ISwap(0xB1BC9f56103175193519Ae1540A0A4572b1566F6);
     }
+
+    function WELLToWormholeUSDC(uint256 amount)
+        external
+        returns (uint256 amountOut)
+    {}
+
+    function GLMRtoWormholeUSDC(uint256 amount)
+        external
+        payable
+        returns (uint256 amountOut)
+    {}
 
     function axlUSDCtoWormholeUSDC(uint256 amount)
         external
@@ -89,5 +92,42 @@ contract Converter {
         );
 
         tokens.axlUSDC.transfer(msg.sender, amountOut);
+    }
+
+    function _convertToken(
+        ERC20 from,
+        ERC20 to,
+        uint256 amount,
+        address[] memory path
+    ) internal returns (uint256) {
+        if (address(from) == address(0)) {
+            tokens.WGLMR.deposit{value: amount}();
+
+            if (address(to) == address(tokens.WGLMR)) {
+                return amount;
+            }
+
+            return _swapTokensUniV2(ERC20(address(tokens.WGLMR)), amount, path);
+        }
+
+        return _swapTokensUniV2(from, amount, path);
+    }
+
+    function _swapTokensUniV2(
+        ERC20 from,
+        uint256 amount,
+        address[] memory path
+    ) internal returns (uint256 amountBought) {
+        // Approve the solarRouter to spend the contract's `from` token.
+        from.approve(address(routers.V2), amount);
+        uint256 lastInPath = path.length - 1;
+        // Swap the tokens through solarRouter
+        amountBought = routers.V2.swapExactTokensForTokens(
+            amount,
+            1,
+            path,
+            address(this),
+            block.timestamp
+        )[lastInPath];
     }
 }
